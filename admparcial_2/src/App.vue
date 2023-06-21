2acb92c330e350d6467df7e92bfee3be43806cc2
marascioni <111396312+marascioni@users.noreply.github.com>
2023-06-21 19:41:16 -0300

    feat: add new welcome message

--- admparcial_2/src/App.vue
+++ admparcial_2/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Estamos probndo vue2 via cli"/>
   </div>
 </template>
 
