afc95299f9b42846cbf7eb8074563f07ec058312
cesaurra <carolina.saurrales@yahoo.com.ar>
2023-06-26 14:28:37 -0300

    refactor: homePage add CardObra component

--- admparcial_2/src/components/HomePage.vue
+++ admparcial_2/src/components/HomePage.vue
@@ -39,26 +39,7 @@
             md="4"
           >
             <v-item>
-              <v-card class="mx-auto" max-width="360">
-                <v-card-title class="titulo">{{ obra.nombre }}</v-card-title>
-                <v-img
-                  :src="obra.portada"
-                  :alt="obra.alt"
-                  height="200px"
-                ></v-img>
-                <v-expand-transition>
-                  <div>
-                    <v-divider></v-divider>
-
-                    <v-card-text>
-                      <p><span>Autor:</span> {{ obra.autor }}</p>
-                      <p><span>Categoría:</span> {{ obra.categoria }}</p>
-                      <p><span>Estilo:</span> {{ obra.estilo }}</p>
-                      <p><span>Año:</span> {{ obra.anio }}</p>
-                    </v-card-text>
-                  </div>
-                </v-expand-transition>
-              </v-card>
+              <CardObra :obra="obra"></CardObra>
             </v-item>
           </v-col>
         </v-row>
@@ -70,11 +51,13 @@
 
 <script>
 import FooterPage from "@/components/FooterPage.vue";
+import CardObra from "@/components/CardObra.vue";
 
 export default {
   name: "HomePage",
   components: {
     FooterPage,
+    CardObra,
   },
   data: () => ({
     show: true,
