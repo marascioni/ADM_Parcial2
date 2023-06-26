de23b843984f4363ec950b5a0fff6ea9881bdd69
cesaurra <carolina.saurrales@yahoo.com.ar>
2023-06-26 16:38:52 -0300

    feat: add login dialog

--- admparcial_2/src/App.vue
+++ admparcial_2/src/App.vue
@@ -4,10 +4,10 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-navigation-drawer v-model="drawer" absolute left temporary>
-        <v-list-item>
+        <v-list-item @click="showDialog">
           <v-list-item-content>
             <v-list-item-title class="text-h6"> SauRa </v-list-item-title>
-            <v-list-item-subtitle> Galería </v-list-item-subtitle>
+            <v-list-item-subtitle> Inicio sesión </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
 
@@ -30,6 +30,58 @@
         </v-list>
       </v-navigation-drawer>
 
+
+
+      <template>
+        <v-row justify="center">
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ }">
+              <v-btn></v-btn>
+            </template>
+            <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Inicio de sesión</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="email"
+                            label="Email*"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="password"
+                            label="Password*"
+                            type="password"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-checkbox label="Politicas de privacidad*"></v-checkbox>
+                          <v-checkbox label="Términos y condiciones*"></v-checkbox>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <small>*campos obligatorios</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                      Cerrar
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="login">
+                      Iniciar Sesión
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+          </v-dialog>
+        </v-row>
+      </template>
+
       <v-main>
         <router-view />
       </v-main>
@@ -41,12 +93,16 @@
 export default {
   name: "App",
   data: () => ({
+    //dialog login
+    email:"",
+    password:"",
+    dialog:false, 
+    ///
     drawer: false,
     items: [
       { title: "Home", icon: "mdi-home", link: "/" },
-      { title: "Nueva Obra", icon: "mdi-image", link: "/agregar" },
-    ],    
-    right: null,
+      { title: "Nueva Obra", icon: "mdi-image", link: "/agregar" },      
+    ],  
     categorias: [
       {
         id: 1,
@@ -115,35 +171,42 @@ export default {
         autor: "Coppola Horacio",
       },
     ],
-    
   }),
-/*******************************************/
-/* METODOS */
-/*******************************************/
+  /*******************************************/
+  /* METODOS */
+  /*******************************************/
   beforeMount() {
     console.log("Se va a montar el componente");
-    if (localStorage.Catalogo!=undefined) {
+    if (localStorage.Catalogo != undefined) {
       this.catalogoObras = JSON.parse(localStorage.getItem("Catalogo"));
-    }else{
-        localStorage.setItem("Catalogo", JSON.stringify(this.catalogoObras));
+    } else {
+      localStorage.setItem("Catalogo", JSON.stringify(this.catalogoObras));
     }
-    if (localStorage.Categoria!=undefined) {
+    if (localStorage.Categoria != undefined) {
       this.categorias = JSON.parse(localStorage.getItem("Categoria"));
-    }else{
-        localStorage.setItem("Categoria", JSON.stringify(this.categorias));
-    }    
+    } else {
+      localStorage.setItem("Categoria", JSON.stringify(this.categorias));
+    }
   },
-
+  methods: {
+    login() {
+      console.log("hola");
+    },
+    showDialog(){
+      console.log("login");
+      this.dialog=true;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #462c50;
 }
 
 nav {
